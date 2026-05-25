450ec4c864f8e55c1088d32488d02632b351a8a1
imdogkung3 <imdogkung3@gmail.com>
2026-05-25 08:45:19 +0700

    Update FunctionsHandler.lua

--- FunctionsHandler.lua
+++ FunctionsHandler.lua
@@ -1,40 +1,116 @@
-FunctionsHandler = {
+local FunctionsHandler = {
     Initalized = false
 }
 
 setmetatable(FunctionsHandler, {
-    __index = function(x, y)
-        QueryResult = rawget(x, y)
+    index = function(Self, Name)
+        local Result = rawget(Self, Name)
 
-        if not QueryResult then
-            return {
-                Register = function(D)
-                    if D == false then
-                        return
+        if not Result then
+            Result = {
+                CacheListener = {},
+                RealCache = {},
+                Methods = {},
+                Constants = {},
+                Events = {},
+                Initalized = true
+            }
+
+            function Result.Register(State)
+                if State == false then
+                    return
+                end
+
+                return true
+            end
+
+            function Result.RegisterMethod(SelfData, MethodName, Callback)
+                SelfData.Methods[MethodName] = {
+                    Name = MethodName,
+                    Callback = Callback,
+
+                    Call = function(MethodData, ...)
+                        return MethodData.Callback(...)
+                    end,
+
+                    Events = {}
+                }
+
+                return true
+            end
+
+            function Result.SaveConstant(SelfData, ConstantName, ConstantValue)
+                if SelfData.Constants[ConstantName] then
+                    return error("constant name was used before!")
+                end
+
+                SelfData.Constants[ConstantName] = ConstantValue
+            end
+
+            function Result.Set(SelfData, CacheName, Value)
+                local Event = SelfData.Events[CacheName]
+
+                if Event then
+                    Event(CacheName, Value)
+                end
+
+                SelfData.RealCache[CacheName] = Value
+
+                return Value
+            end
+
+            function Result.Get(SelfData, CacheName)
+                return SelfData.Constants[CacheName]
+                    or SelfData.RealCache[CacheName]
+            end
+
+            Result.CacheListener.Parent = Result
+
+            setmetatable(Result.CacheListener, {
+                newindex = function(Table, CacheName, Value)
+                    local Parent = Table.Parent
+                    local Event = Parent.Events[CacheName]
+
+                    if Event then
+                        Event(CacheName, Value)
                     end
 
-                    Result = {
-                        CacheListener = {},
-                        RealCache = {},
-                        Methods = {},
-                        Constants = {},
-                        Events = {},
-                        Initalized = true
-                    }
+                    Parent.RealCache[CacheName] = Value
+                end
+            })
 
-                    function Result.RegisterMethod(J, K, L)
-                        J.Methods[K] = {
-                            Name = K,
-                            Callback = L,
+            rawset(Self, Name, Result)
+        end
 
-                            Call = function(M, ...)
-                                return M.Callback(...)
-                            end,
+        return Result
+    end
+})
 
-                            Events = {}
-                        }
-
-                        return true
+FunctionsHandler.LocalPlayerController.Register()
+FunctionsHandler.ExpRedeem.Register()
+FunctionsHandler.LevelFarm.Register()
+FunctionsHandler.SniperFruit.Register()
+FunctionsHandler.Pole.Register()
+FunctionsHandler.Saber.Register()
+FunctionsHandler.Yama.Register()
+FunctionsHandler.Tushita.Register()
+FunctionsHandler.CursedDualKatana.Register()
+FunctionsHandler.SkullGuitar.Register()
+FunctionsHandler.PullLever.Register()
+FunctionsHandler.SecondSeaPuzzle.Register()
+FunctionsHandler.ColosseumPuzzle.Register()
+FunctionsHandler.Trevor.Register()
+FunctionsHandler.EvoRaceV2.Register()
+FunctionsHandler.EvoRaceV3.Register()
+FunctionsHandler.ThirdSeaPuzzle.Register()
+FunctionsHandler.PirateRaid.Register()
+FunctionsHandler.RaidController.Register()
+FunctionsHandler.MeleesController.Register()
+FunctionsHandler.BossesTask.Register()
+FunctionsHandler.SpecialBossesTask.Register()
+FunctionsHandler.CollectFruit.Register()
+FunctionsHandler.CollectBerry.Register()
+FunctionsHandler.UtillyItemsActivitation.Register()                        return true
                     end
 
                     setmetatable(Result.Constants, {
