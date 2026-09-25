fcfce7b8770eea1df6aa5e318caeee795945d964
imdogkung3 <imdogkung3@gmail.com>
2026-09-25 14:41:05 +0700

    Create Option.lua

---
+++ Module/Option.lua
@@ -0,0 +1,45 @@
+local _ENV = (getgenv or getrenv or getfenv)()
+
+local RunService = game:GetService("RunService")
+local Heartbeat = RunService.Heartbeat
+
+local Module = {}
+
+Module.Connect = (function()
+    local Connections = _ENV.Connections or {}
+
+    for i = 1, #Connections do
+        Connections[i]:Disconnect()
+    end
+
+    table.clear(Connections)
+    _ENV.Connections = Connections
+
+    return function(Instance, Callback)
+        local Connection = Instance:Connect(Callback)
+
+        table.insert(Connections, Connection)
+
+        return Connection
+    end
+end)()
+
+local Connect = Module.Connect
+
+function , Interval)
+    Interval = Interval or 0.1
+
+    Connect(Heartbeat, function()
+        task.wait(Interval)
+
+        if Function then
+            local Success, Error = pcall(Function)
+
+            if not Success then
+                print(Error)
+            end
+        end
+    end)
+end
+
+return Module
